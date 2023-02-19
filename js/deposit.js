51002524e3eb4ccae074cddba7baaaf89f0ee74c
Pranto <pranto15-12662@diu.edu.bd>
2023-02-19 18:21:15 +0600

    new up

--- js/deposit.js
+++ js/deposit.js
@@ -1,10 +1,14 @@
 document.getElementById('btn-deposit').addEventListener('click', function () {
 
     const depositFiled = document.getElementById('user-deposit');
-
     const deposit = depositFiled.value;
-
     const depositNumber = parseFloat(deposit);
+    depositFiled.value = '';
+
+    if(isNaN(depositNumber)){
+        alert('Please Input Valid Number');
+        return;
+    }
 
     const addDeposit = document.getElementById('add-deposit');
 
@@ -21,5 +25,5 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
     const balance = totalBalance + depositNumber;
     balanceFiled.innerText = balance;
 
-    depositFiled.value = '';
+    
 })
